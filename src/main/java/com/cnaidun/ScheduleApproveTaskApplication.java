9a8a51840f550b5e1840f4a8e68c9ff3e5464f42
yonghz <yonght@51kezhi.com>
2018-10-30 16:26:09 +0800

    新增任务呀

---
+++ src/main/java/com/cnaidun/ScheduleApproveTaskApplication.java
@@ -0,0 +1,20 @@
+package com.cnaidun;
+
+//import org.activiti.spring.boot.SecurityAutoConfiguration;
+import com.cnaidun.user.api.scheduleApproveTask.config.WxReceiver;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@SpringBootApplication
+
+@WebAppConfiguration
+@MapperScan({"com.cnaidun.user.api.scheduleApproveTask.mapper","com.cnaidun.messageclient.controller"})
+public class ScheduleApproveTaskApplication {
+//@EnableFeignClients
+	public static void main(String[] args) {
+				SpringApplication.run(ScheduleApproveTaskApplication.class, args);
+	}
+}
